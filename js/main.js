2bb2847e847841adf9102246effcd3c040563503
Uzair Ashraf <uzinatorcl@live.com>
2019-09-17 10:23:17 -0700

    yelp api in process, main.js created

--- js/main.js
+++ js/main.js
@@ -10,5 +10,6 @@ function retrieveUserPositon(data) {
   console.log(data);
   userPositionLat = data.coords.latitude;
   userPositionLong = data.coords.longitude;
-  console.log(`The latitude is ${userPositionLat} and the longitude is ${userPositionLong}`)
+  console.log(`The latitude is ${userPositionLat} and the longitude is ${userPositionLong}`);
+  const yelpObject = new Yelp(userPositionLat, userPositionLong);
 }
--- js/yelp.js
+++ js/yelp.js
@@ -3,21 +3,30 @@ class Yelp {
     this.apiKey = 'Bearer a_BrDbXlVK8u3TbVbpFRC9EP6Ye_73iUJQvTRDbJBrbD_e0t9x4OqWni0XZK8hE_VLr2GLWHBfgrEDdY6jZO16i1Gq5tMTBIBczxbqU1e2P3-cOOmkTUVgNE0TiAXXYx';
     this.userLatitude = userLatitude;
     this.userLongitude = userLongitude;
+    this.processData = this.processData.bind(this);
+    this.errorProcessingData = this.errorProcessingData.bind(this);
+    this.render();
   }
 
   render() {
-
+    this.retrieveData(this.userLatitude, this.userLongitude);
   }
 
   retrieveData(latitude, longitude) {
     $.ajax({
       'url': `https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}`,
-      'method' : 'GET',
       'headers': {
-        "Authorization" : this.apiKey,
-
-      }
+        "Authorization" : this.apiKey
+      },
+      success: this.processData,
+      error: this.errorProcessingData
     })
   }
+  processData(data) {
+    console.log(data);
+  }
 
+  errorProcessingData() {
+    alert('There was an error processing your data.');
+  }
 }
