2a36f603dc3af64bd7d26289b0a15e2a840c951d
Heondo Kim <heondokim@gmail.com>
2019-09-17 11:29:05 -0700

    merging for main js refactor

--- js/maps.js
+++ js/maps.js
@@ -1,7 +1,31 @@
+class googleMap {
+  constructor(lat, lng){
+    this.lat = lat,
+    this.lng = lng
+    this.mapObj = null;
+  }
+
+  render() {
+    const map = new google.maps.Map(document.getElementById('mapDisplay'), {
+      zoom: 14,
+      center: {
+        lat: this.lat,
+        lng: this.lng
+      },
+      mapTypeId: 'terrain'
+    });
+    this.mapObj = map;
+  }
+
+  // addMarkers(businesses)
+}
+
+
 
 class Marker {
-  constructor(map, position, business){
+  constructor(map, position, business, type){
     this.clickHandler = this.clickHandler.bind(this)
+    this.type = type;
     this.business = business;
     this.map = map;
     this.marker = null;
@@ -20,8 +44,7 @@ class Marker {
   }
 
   clickHandler = () => {
-    // we should take in some position
+    // should open up information about
     console.log(this.business.name);
-
   }
 }
