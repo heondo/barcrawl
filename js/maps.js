72a31becacf1ac2bce9a1c1335b9aa38fbb9a5ba
Heondo Kim <heondokim@gmail.com>
2019-09-17 09:51:02 -0700

    basic maps to work, working on custom markers object

--- js/maps.js
+++ js/maps.js
@@ -1,20 +1,108 @@
-class Map {
-  constructor(position) {
-    this.position = {
-      lat: 33.637336,
-      lng: -117.739905
-    // POSITION};
+function initMap() {
+  const map = new google.maps.Map(document.getElementById('mapDisplay'), {
+    zoom: 14,
+    center: {
+      lat: 33.637,
+      lng: -117.739 },
+    mapTypeId: 'terrain'
+  });
+
+  const marker1 =
+
+}
+
+class newMarker {
+  constructor(map, position, business){
+    this.business = business;
+    this.map = map;
+    this.marker = null;
+    this.position = position;
   }
-    this.map = null;
-  }
-  generateMap(){
-    let map = new google.maps.Map($(".mapDisplay"), {
-      center: {
-        lat: this.position.lat,
-        lon: this.position.lon
-      },
-      zoom: 8
-    });
-    console.log()
+
+  render() {
+    this.marker = new google.maps.Marker({
+      position: this.position,
+      map: this.map,
+      title: "hello"
+    })
   }
 }
+
+
+// class Map {
+//   constructor(position) {
+//     this.initMap = this.initMap.bind(document)
+//     this.position = {
+//       lat: 33.637,
+//       lng: -117.739
+//     };
+//     this.api_key = "AIzaSyCWq6apxh7IJs8njuJgCEJf5QPenKjrCYc";
+//   }
+
+//   initMap(lat, lng){
+//     const s = document.createElement("script");
+//     document.head.append(s);
+//     let map;
+//     s.addEventListener("load", () => {
+//       map = new google.maps.Map(document.getElementById('mapDisplay'), {
+//         zoom: 14,
+//         center: {
+//           lat: lat,
+//           lng: lng
+//         },
+//         mapTypeId: 'terrain'
+//       })
+//     });
+//     s.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCWq6apxh7IJs8njuJgCEJf5QPenKjrCYc"
+//   }
+
+// }
+
+
+// class Map {
+//   constructor(position) {
+//     // this.initMap = this.initMap.bind(this);
+//     this.position = {
+//       lat: 33.637,
+//       lng: -117.739
+//     // POSITION};
+//   }
+//     this.map = null;
+//   }
+//   // generateMap(){
+//   //   console.log(this.position.lat, this.position.lng)
+//   //   let map = new google.maps.Map(document.getElementById('mapDisplay'), {
+//   //     center: {
+//   //       lat: this.position.lat,
+//   //       lng: this.position.lng
+//   //     },
+//   //     zoom: 8
+//   //   });
+//   //   console.log(map)
+//   // }
+
+//   initMap() {
+//     // let s = $("<script>");
+//     // s.on("load", () => {
+//     //   this.map = new google.maps.Map(document.getElementById('mapDisplay'), {
+//     //     center: {
+//     //       lat: this.position.lat,
+//     //       lng: this.position.lng
+//     //     },
+//     //     zoom: 8
+//     //   });
+//     //   s.attr("src", "https://maps.googleapis.com/maps/api/js?key=AIzaSyCWq6apxh7IJs8njuJgCEJf5QPenKjrCYc");
+//     // });
+//     // // s.attr("src", "https://maps.googleapis.com/maps/api/js?key=AIzaSyCWq6apxh7IJs8njuJgCEJf5QPenKjrCYc");
+//     // $("body").append(s)
+//     // $("<script>")
+//     var map = new google.maps.Map(document.getElementById('mapDisplay'), {
+//       center: {
+//         lat: 33.637,
+//         lng: -117.739
+//        },
+//       zoom: 8
+//     });
+//     // console.log(map)
+//   }
+// }
