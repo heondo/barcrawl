83d2dd6110ac4ec74b4a08182c4251563b5c7f42
Heondo Kim <heondokim@gmail.com>
2019-09-17 18:07:54 -0700

    not behind on dev

--- js/marker.js
+++ js/marker.js
@@ -1,7 +1,9 @@
 class Marker {
-  constructor(map, data) {
+  constructor(map, data, domElement, updateCenterCallback) {
     this.eventClickHandler = this.eventClickHandler.bind(this)
     this.bizClickHandler = this.bizClickHandler.bind(this)
+    this.updateCenterCallback = updateCenterCallback;
+    this.domElement = $(domElement);
     this.data = data;
     this.map = map;
     this.type = null;
@@ -16,6 +18,8 @@ class Marker {
       lat: parseFloat(event.venue.address.latitude),
       lng: parseFloat(event.venue.address.longitude)
     }
+    this.position = position;
+
     const icon = {
       url: "assets/images/icons8-event-64.png", // url
       scaledSize: new google.maps.Size(35, 35), // scaled size
@@ -40,7 +44,7 @@ class Marker {
       lat: parseFloat(biz.coordinates.latitude),
       lng: parseFloat(biz.coordinates.longitude)
     }
-
+    this.position = position;
     const icon = {
       url: "assets/images/icons8-beer-48.png", // url
       scaledSize: new google.maps.Size(35, 35), // scaled size
@@ -60,17 +64,13 @@ class Marker {
         this.bizClickHandler)
   }
 
-  eventClickHandler() {
+  eventClickHandler = () => {
     // should open up information about
-    // console.log(this.name);
-    let infoWindow = new google.maps.InfoWindow({
-      content: `<div> ${this.name}</div>`
-    })
-    // console.log(infoWindow, this.marker)
-    infoWindow.open(this.map)
+   this.updateCenterCallback(this.position);
+    // this.map.setCenter(this.position)
   }
 
   bizClickHandler = () => {
-    console.log(this.name)
+    this.updateCenterCallback(this.position);
   }
 }
