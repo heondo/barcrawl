89b452f0e160503cfeadfd0dd7aca238186c8995
Heondo Kim <heondokim@gmail.com>
2019-09-17 20:53:12 -0700

    add user location to Map

--- js/maps.js
+++ js/maps.js
@@ -6,13 +6,15 @@ class googleMap {
     // this.biz = biz,
     this.setMapOnAll = this.setMapOnAll.bind(this);
     this.clearMarkers = this.clearMarkers.bind(this);
+    this.clearMarkers = this.clearMarkers.bind(this);
     this.updateLocation = this.updateLocation.bind(this)
     this.lat = lat,
-      this.lng = lng,
-      this.mapObj = null
+    this.lng = lng,
+    this.mapObj = null
     this.markers = {
       events: [],
-      biz: []
+      biz: [],
+      user: null
     }
   }
 
@@ -26,11 +28,19 @@ class googleMap {
       }
       // mapTypeId: 'terrain',
     });
+
     this.mapObj = map;
+
+    const userMarker = new Marker(this.mapObj, {name: "You"}, undefined, this.updateLocation, this.closeWindows);
+    userMarker.renderUser({
+      lat: this.lat,
+      lng: this.lng
+    });
+    this.markers.user = userMarker;
   }
 
   updateLocation(position) {
-    this.mapObj.setCenter(position)
+    this.mapObj.panTo(position);
   }
 
   setMapOnAll(map) {
@@ -57,12 +67,23 @@ class googleMap {
     this.setMapOnAll(null);
   }
 
+  closeWindows = () => {
+    for (let eventMarker of this.markers.events) {
+      console.log(eventMarker)
+      eventMarker.infoWindow.close(this.map);
+      // marker.marker.setMap(null);
+    }
+    for (let bizMarker of this.markers.biz) {
+      bizMarker.infoWindow.close(this.map);
+    }
+  }
+
 
   addEvents(events) {
     // takes in the array data from eventbrite response and creates/renders Markers
     // on the map
     events.map((event, index) => {// add .
-      const eventMarker = new Marker(this.mapObj, event, `.event${index}`, this.updateLocation);
+      const eventMarker = new Marker(this.mapObj, event, `.event${index}`, this.updateLocation, this.closeWindows);
       this.markers.events.push(eventMarker);
       eventMarker.renderEvent(event);
     });
@@ -71,7 +92,7 @@ class googleMap {
 
   addBiz(businesses) {
     businesses.map((biz, index) => {
-      const bizMarker = new Marker(this.mapObj, biz, `.business${index}`, this.updateLocation);
+      const bizMarker = new Marker(this.mapObj, biz, `.business${index}`, this.updateLocation, this.closeWindows);
       this.markers.biz.push(bizMarker);
       bizMarker.renderBiz(biz);
     })
