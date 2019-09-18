3602e0efd53ce072260c5d23c25c28a1ca224d68
Edward W. Lee <elee5696@gmail.com>
2019-09-17 20:53:43 -0700

    autocomplete box added

--- js/maps.js
+++ js/maps.js
@@ -27,6 +27,7 @@ class googleMap {
       // mapTypeId: 'terrain',
     });
     this.mapObj = map;
+    this.initAutocomplete();
   }
 
   updateLocation(position) {
@@ -77,4 +78,61 @@ class googleMap {
     })
   }
   // addMarkers(businesses)
+
+
+  initAutocomplete() {
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    this.mapObj.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', () => {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach( (marker) => {
+        marker.setMap(null);
+      });
+      markers = [];
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach( (place) => {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: this.mapObj,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      this.mapObj.fitBounds(bounds);
+    });
+  }
+
 }
